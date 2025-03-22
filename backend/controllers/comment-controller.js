2aaa13d234b4535888cfeac30d07da6f1c05af3f
Hòn <danghong0521@gmail.com>
2025-03-22 19:41:20 +0700

    api create get comment

--- backend/controllers/comment-controller.js
+++ backend/controllers/comment-controller.js
@@ -1,3 +1,86 @@
 const Comment = require('../models/commentSchema')
 
-module.exports = {}
+//api tạo 
+const createComment = async (req, res) => {
+    const userId = req.user.id;
+    const userType = req.user.role;
+    const { content, images, newsId, parentId } = req.body
+    try {
+        if (!content) {
+            return res.status(400).json({ message: 'Content is required' })
+        }
+
+        const comment = await Comment.create({
+            userId,
+            userType,
+            content,
+            images,
+            newsId,
+            parentId
+        })
+
+        res.status(201).json(comment)
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', error })
+    }
+}
+
+// Lấy danh sách bình luận theo bài viết (getCommentsByNews)
+const getCommentsByNews = async (req, res) => {
+    try {
+        const { newsId } = req.params
+        console.log('newsId', newsId)
+        const comments = await Comment.find({ newsId })
+            .populate('userId', 'name') // Lấy thông tin người dùng
+            .sort({ createdAt: -1 }) // Sắp xếp theo thời gian mới nhất
+
+        res.status(200).json(comments)
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', error })
+    }
+}
+
+const getCommentDetails = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const comment = await Comment.findById(id)
+            .populate({ path: 'userId', select: 'name' })  // Lấy thông tin người dùng
+
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+
+        res.status(200).json(comment);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', error });
+    }
+};
+
+const getCommentsByParentComment = async (req, res) => {
+    try {
+        const { parentId } = req.params;
+
+        const comments = await Comment.find({ parentId })
+            .populate({ path: 'userId', select: 'name' }) // Lấy thông tin người dùng
+            .populate({
+                path: 'userId',
+                refPath: 'userRef',
+                select: 'name'
+            }) // Lấy thông tin người dùng dựa trên userType
+            .sort({ createdAt: -1 }); // Sắp xếp theo thời gian mới nhất
+
+        res.status(200).json(comments);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', error });
+    }
+};
+
+module.exports = {
+    createComment,
+    getCommentsByNews,
+    getCommentDetails,
+    getCommentsByParentComment
+    // updateComment,
+    // deleteComment
+}
--- backend/routes/comment.router.js
+++ backend/routes/comment.router.js
@@ -1,5 +1,22 @@
 const express = require('express')
-const {} = require('../controllers/comment-controller')
-commentRouter = express.Router()
+const { authorizeUser } = require('../middlewares/auth')
+const {
+    createComment,
+    getCommentsByNews,
+    getCommentDetails,
+    getCommentsByParentComment
+
+    //   updateComment,
+    //   deleteComment
+} = require('../controllers/comment-controller')
+
+const commentRouter = express.Router()
+commentRouter.post('/', authorizeUser, createComment),
+    commentRouter.get('/news/:newsId', getCommentsByNews) // Lấy bình luận theo bài viết
+commentRouter.get('/:id', getCommentDetails) // Lấy chi tiết bình luận
+commentRouter.get('/parent-comment/:id', getCommentsByParentComment) // Lấy chi tiết bình luận
+
+// commentRouter.put('/:id', authorizeUser, updateComment)
+// commentRouter.delete('/:id', authorizeUser, deleteComment)
 
 module.exports = commentRouter
