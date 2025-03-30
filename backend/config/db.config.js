bdb861007717051c3a2e3b8868c77ed64729ee5e
HaiBarca03 <doanduchai8d2m@gmail.com>
2025-03-30 11:56:32 +0700

    change url db

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-const connectDB = async () => {
+dotenv.config()
+
+const dbConnect = async () => {
+  const dbUrl =
+    'mongodb+srv://duchai:duchai@mongodb.uv8cn.mongodb.net/StudentManagement?retryWrites=true&w=majority&appName=MongoDB'
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(dbUrl, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000
+      useUnifiedTopology: true
     })
     console.log('✅ Database connected successfully')
-  } catch (err) {
-    console.error('❌ Database connection failed:', err)
+  } catch (error) {
+    console.error('❌ Database connection failed:', error.message)
     process.exit(1)
   }
 }
 
-module.exports = connectDB
+module.exports = dbConnect
